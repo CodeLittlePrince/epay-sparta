c8d86ec327da1b8a538a72ce15c962c7e9f534a7
倪俊杰 <nijunjie@corp.netease.com>
2021-08-16 20:53:01 +0800

    chore: 删除不需要声明的plugins

--- packages/epay-sparta-service/lib/config/babel.config.js
+++ packages/epay-sparta-service/lib/config/babel.config.js
@@ -10,7 +10,6 @@ module.exports = {
     ]
   ],
   'plugins': [
-    '@babel/plugin-syntax-dynamic-import',
     ['@babel/plugin-proposal-object-rest-spread', { 'loose': true, 'useBuiltIns': true }], // babel-preset-env已依赖安装
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     ['@babel/plugin-proposal-private-methods', { 'loose': true }]
